91f974cd16b467608c52dd85468ccb43dd90489c
sys <1425546057@qq.com>
2020-11-04 23:12:29 +0800

    新建项目

---
+++ git_test_idea/src/com/deyuan/test.java
@@ -0,0 +1,17 @@
+package com.deyuan;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+        System.out.println("啥代理费玛莎拉蒂开阀门拉什么地方了撒开的");
+    }
+}
